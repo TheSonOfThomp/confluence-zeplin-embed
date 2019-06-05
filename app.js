918f80b74042b5dc605f2384f38d1a623bde64cd
Adam Thompson <athompson@newvisions.org>
2019-06-05 12:31:13 -0400

    # This is a combination of 4 commits. # This is the 1st commit message:
    refactor app.js
    
    # This is the commit message #2:
    
    move devDependencies
    
    # This is the commit message #3:
    
    remove auth
    
    # This is the commit message #4:
    
    add credentials

--- app.js
+++ app.js
@@ -3,66 +3,82 @@
 
 // [Express](http://expressjs.com/) is your friend -- it's the underlying
 // web framework that `atlassian-connect-express` uses
-var express = require('express');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-var cookieParser = require('cookie-parser');
-var errorHandler = require('errorhandler');
-var morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('errorhandler');
+const morgan = require('morgan');
+
 // You need to load `atlassian-connect-express` to use her godly powers
-var ac = require('atlassian-connect-express');
+const ace = require('atlassian-connect-express');
+
 // Static expiry middleware to help serve static resources efficiently
-process.env.PWD = process.env.PWD || process.cwd(); // Fix expiry on Windows :(
-var expiry = require('static-expiry');
+// process.env.PWD = process.env.PWD || process.cwd(); // Fix expiry on Windows :(
+// const expiry = require('static-expiry');
+
 // We use [Handlebars](http://handlebarsjs.com/) as our view engine
 // via [express-hbs](https://npmjs.org/package/express-hbs)
-var hbs = require('express-hbs');
+const hbs = require('express-hbs');
+
 // We also need a few stock Node modules
-var http = require('http');
-var path = require('path');
-var os = require('os');
+const http = require('http');
+const path = require('path');
+const os = require('os');
 
-// Anything in ./public is served up as static content
-var staticDir = path.join(__dirname, 'public');
-// Anything in ./views are HBS templates
-var viewsDir = __dirname + '/views';
 // Your routes live here; this is the C in MVC
-var routes = require('./routes');
-// Bootstrap Express
-var app = express();
-// Bootstrap the `atlassian-connect-express` library
-var addon = ac(app);
-// You can set this in `config.json`
-var port = addon.config.port();
-// Declares the environment to use in `config.json`
-var devEnv = app.get('env') == 'development';
+const routes = require('./routes');
 
-// The following settings applies to all environments
+// Bootstrap Express
+const app = express();
+// Bootstrap the `atlassian-connect-express` library
+const addon = ace(app);
+
+// See config.json
+const port = addon.config.port();
 app.set('port', port);
 
-// Configure the Handlebars view engine
+
+// Configure Handlebars
+// Anything in ./views are HBS templates
+const viewsDir = __dirname + '/views';
 app.engine('hbs', hbs.express3({partialsDir: viewsDir}));
+// app.engine('hbs', hbs.express4({ partialsDir: viewsDir }));
+// Configure the Handlebars view engine
 app.set('view engine', 'hbs');
 app.set('views', viewsDir);
 
+// You can set this in `config.json`
+
+// Declares the environment to use in `config.json`
+const devEnv = app.get('env') == 'development';
+app.use(morgan(devEnv ? 'dev' : 'combined'));
+
+
+
 // Declare any Express [middleware](http://expressjs.com/api.html#middleware) you'd like to use here
 // Log requests, using an appropriate formatter by env
-app.use(morgan(devEnv ? 'dev' : 'combined'));
 // Include request parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+
 // Gzip responses when appropriate
 app.use(compression());
 
 // You need to instantiate the `atlassian-connect-express` middleware in order to get its goodness for free
 app.use(addon.middleware());
+
 // Enable static resource fingerprinting for far future expires caching in production
-app.use(expiry(app, {dir: staticDir, debug: devEnv}));
-// Add an hbs helper to fingerprint static resource urls
-hbs.registerHelper('furl', function(url){ return app.locals.furl(url); });
+// app.use(expiry(app, {dir: staticDir, debug: devEnv}));
+
+// Anything in ./public is served up as static content
+var staticDir = path.join(__dirname, 'public');
 // Mount the static resource dir
 app.use(express.static(staticDir));
+
+// Add an hbs helper to fingerprint static resource urls
+// hbs.registerHelper('furl', function(url){ return app.locals.furl(url); });
 
 // Show nicer errors when in dev mode
 if (devEnv) app.use(errorHandler());
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ module.exports = function (app, addon) {
 
     // Render the macro by returning html generated from the hello-world template.
     // The hello-world template is defined in /views/hello-world.hbs.
-    app.get('/macro', addon.authenticate(), function (req, res) {
+    app.get('/macro', function (req, res) {
       var zeplinUrl = req.query['zeplinUrl']
 
       const projectID = zeplinUrl.substring(
